dde417de353eb1121bf59d52f216fe716793cce4
D068314 <burak.ayyildiz@sap.com>
2023-01-04 17:46:00 +0100

    feat: add profiles view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,11 +18,16 @@ from django.urls import include, path
 from users import views
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("food/", include("food.urls")),
     path("register/",views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout")
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path("profile/", views.profile, name="profile")
 ]
+
+urlpatterns += [] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm
 
@@ -16,3 +17,7 @@ def register(request):
        messages.success(request, "Welcome {} , dont loose your pw {} and thanks for registering".format(username, password))
        return redirect("login")
   return render(request, "users/register.html", {"form":form})
+
+@login_required(login_url="login")
+def profile(request):
+  return render(request, "users/profile.html")
