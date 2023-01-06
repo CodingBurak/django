78037304f44e41e81a9a0bc582107686b08ad5c5
D068314 <burak.ayyildiz@sap.com>
2023-01-06 19:29:20 +0100

    feat: class based views

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -4,13 +4,17 @@ from . import views
 app_name = "food"
 urlpatterns = [
     #/food
-    path("", views.index, name = "index"),
-    path("item", views.item, name="item"),
+    #path("", views.index, name = "index"),
+    path("", views.IndexClassView.as_view(), name= "index"),
+   # path("item", views.item, name="item"),
     #"food/id"
-    path("<int:item_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailClassView.as_view(), name="detail"),
+    #path("<int:item_id>/", views.detail, name="detail"),
     #add
-    path("add", views.create_item, name="create_item"),
+    path("add", views.CreateItemView.as_view(), name="create_item"),
     #edit
     path("update/<int:item_id>", views.update_item, name="update_item"),
-    path("delete/<int:item_id>", views.delete_item, name="delete_item")
+    #delete
+   # path("delete/<int:item_id>", views.delete_item, name="delete_item")
+    path("delete/<int:pk>", views.ItemDeleteView.as_view(), name="delete_item")
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # function name will be the view
@@ -17,15 +22,27 @@ def index(request):
   #return HttpResponse(temp.render(context, request))
   return render(request, "food/index.html", context=context)
 
+class IndexClassView(ListView):
+  model = Item
+  template_name="food/index.html"
+  context_object_name = "items"
+
+
 def item(request):
   return HttpResponse('<h1>this is an item view</h1>')
 
-def detail(request, item_id):
+
+def detail22(request, item_id):
   item = Item.objects.get(pk=item_id)
   ctx = {
     "item": item
   }
   return render(request, "food/details.html", context=ctx)
+
+class DetailClassView(DetailView):
+  model = Item
+  template_name="food/details.html"
+  context_object_name= "item"
 
 def create_item(request):
   form = ItemForm(request.POST or None)
@@ -36,6 +53,23 @@ def create_item(request):
   
   return render(request, "food/item-form.html",context= {'form': form})
 
+#@login_required(login_url="login")
+class CreateItemView(CreateView):
+    model = Item
+    fields = ["item_name", "item_desc", "item_price", "item_image"]
+    template_name = "food/item-form.html"
+    
+    def form_valid(self, form):
+      form.instance.user_name = self.request.user
+      
+      return super().form_valid(form)
+    
+    #def get_success_url(self) -> str:
+     #   return reverse('food:detail',kwargs={"pk":self.request})
+    
+
+
+
 def update_item(request, item_id):
   item = Item.objects.get(id=item_id)
   form = ItemForm(request.POST or None,instance=item)
@@ -43,6 +77,16 @@ def update_item(request, item_id):
     form.save()
     return redirect("food:index")
   return render(request, "food/item-form.html", context={'form': form})
+
+class UpdateItemView(UpdateView):
+    model = Item
+    context_object_name= "form"
+    template_name = "food/item-form.html"
+    
+    def get_success_url(self) -> str:
+      pass
+
+
   
 def delete_item(request, item_id):
   #Item.objects.delete(id=item_id)
@@ -54,3 +98,12 @@ def delete_item(request, item_id):
   return render(request, "food/item-delete.html", context={"item": item})
   #items = Item.objects.all()
   #return render(request,"food/index.html", {"items": items})
+  
+class ItemDeleteView(DeleteView):
+    model = Item
+    template_name = "food/item-delete.html"
+    context_object_name = "item"
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('food:index')
+  
